29b1c7c85f8128082f2a5531ec60b98207c1ab09
ayaarragab <ayarsk4343@gmail.com>
2023-09-22 12:50:33 +0300

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,36 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - insertion sort for a dbl list
- * @list: ptr to ptr of the list
- * Return: nothing
-*/
+ * insertion_sort_list - sorts a DLL of integers in
+ * ascending order using the insertion sort
+ * algorithm
+ *
+ * @list: Doubly linked list
+ * Return: Null
+ */
+
 void insertion_sort_list(listint_t **list)
 {
-    int value;
-    listint_t *current, *NodeA, *NodeB;
+	listint_t *current, *temp;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    current = (*list)->next;
-
-    while (current)
-    {
-        value = current->n;
-
-        while (current->prev && current->prev->n > value)
-        {
-            NodeA = current;
-            NodeB = current->prev;
-
-            NodeA->prev = NodeB->prev;
-
-            if (NodeB->prev)
-                NodeB->prev->next = NodeA;
-            else
-                *list = NodeA;
-
-            NodeB->next = NodeA->next;
-            NodeA->next = NodeB;
-
-            if (NodeB->next)
-                NodeB->next->prev = NodeB;
-        }
-
-        current = current->next;
-    }
+	if (!list)
+		return;
+	current = *list;
+	while (current)
+	{
+		while (current->next && (current->n > current->next->n))
+		{
+			temp = current->next;
+			current->next = temp->next;
+			temp->prev = current->prev;
+			if (current->prev)
+				current->prev->next = temp;
+			if (temp->next)
+				temp->next->prev = current;
+			current->prev = temp;
+			temp->next = current;
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
