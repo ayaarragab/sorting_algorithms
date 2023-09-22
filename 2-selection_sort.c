3b9874335a332c34d935240149d3d55dcca44398
ayaarragab <ayarsk4343@gmail.com>
2023-09-22 13:33:18 +0300

    task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,6 +9,8 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, index, temp;
 
+	if (!array || size == 0 || size == 1 || size == 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		index = i;
